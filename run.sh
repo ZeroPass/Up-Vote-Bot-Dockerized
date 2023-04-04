2f06249cc7fe59cd1855321f781ec17578f660b6
root <nejko90@gmail.com>
2023-04-04 18:24:36 +0000

    updated run script

--- run.sh
+++ run.sh
@@ -4,5 +4,8 @@ echo =">> Adding folder to PYTHONPATH and LD_LIBRARY_PATH>>"
 export PYTHONPATH=$PYTHONPATH:'/app'
 export LD_LIBRARY_PATH=/app/external/abieos-python/external/abieos/build
 
+#we need this location for correct loading of a picture
+cd /app
+
 echo ">> Start running the script>>"
 python /app/edenBot.py
